70ca5589bb64923b3a914a7065ed433136651d1a
AlbaPCode <albapardo.code@gmail.com>
2024-03-10 18:42:01 +0100

    appmodulets changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'localhost',
+      port:3306,
+      username:'root',
+      password:'Espapo1986+',
+      database:'db_talenthub '
+    }),
+    ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })

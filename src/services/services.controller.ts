f88e5497f357030d0ca8f9497019166f9d4eaf65
AlbaPCode <albapardo.code@gmail.com>
2024-03-11 00:38:21 +0100

    resource Services completed, no error with npm run start

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 import { ServicesService } from './services.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
-
+import { Service } from './entities/service.entity';
 @Controller('api/services')
 export class ServicesController {
   constructor(private readonly servicesService: ServicesService) {}
 
   @Post()
-  create(@Body() createServiceDto: CreateServiceDto) {
+  create(@Body() createServiceDto: CreateServiceDto): Promise<Service> {
     return this.servicesService.create(createServiceDto);
   }
 
+
   @Get()
-  findAll() {
+  findAll(): Promise<Service[]> {
     return this.servicesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.servicesService.findOne(+id);
+  findOne(@Param('id') id: string): Promise<Service> {
+    return this.servicesService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateServiceDto: UpdateServiceDto) {
-    return this.servicesService.update(+id, updateServiceDto);
+  update(@Param('id') id: string, @Body() updateServiceDto: UpdateServiceDto): Promise<Service> {
+    return this.servicesService.update(id, updateServiceDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.servicesService.remove(+id);
+  remove(@Param('id') id: string): Promise<void> {
+    return this.servicesService.remove(id);
   }
 }
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,39 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { CreateServiceDto } from './dto/create-service.dto';
-import { UpdateServiceDto } from './dto/update-service.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Service } from './entities/service.entity';
 import { Repository } from 'typeorm';
-
+import { CreateServiceDto } from './dto/create-service.dto';
+import { UpdateServiceDto } from './dto/update-service.dto';
 
 @Injectable()
 export class ServicesService {
+  constructor(
+    @InjectRepository(Service)
+    private readonly servicesRepository: Repository<Service>,
+  ) {}
 
-constructor(
-  @InjectRepository(Service)
-  private servicesRepository:Repository<Service>,
-){
-
-}
-
-
-  async create(createServiceDto: CreateServiceDto) {
-    return await this.servicesRepository.save(createServiceDto);
+  async create(createServiceDto: CreateServiceDto): Promise<Service> {
+    const newService = this.servicesRepository.create(createServiceDto);
+    return this.servicesRepository.save(newService);
   }
 
-  async findAll() {
-    return await this.servicesRepository.find();
+  async findAll(): Promise<Service[]> {
+    return this.servicesRepository.find();
   }
 
-  async findOne(id: number) {
-    return await `This action returns a #${id} service`;
+  async findOne(id: string): Promise<Service> {
+    return this.servicesRepository.findOne({ where: { service_id: id } });
   }
 
-  async update(id: number, updateServiceDto: UpdateServiceDto) {
-    return await `This action updates a #${id} service`;
+  async update(id: string, updateServiceDto: UpdateServiceDto): Promise<Service> {
+    const existingService = await this.servicesRepository.findOne({ where: { service_id: id } });
+    if (!existingService) {
+      throw new NotFoundException(`Service with ID ${id} not found`);
+    }
+
+    const updatedService = this.servicesRepository.merge(existingService, updateServiceDto);
+    return this.servicesRepository.save(updatedService);
   }
 
-  async remove(id: number) {
-    return await`This action removes a #${id} service`;
+  async remove(id: string): Promise<void> {
+    await this.servicesRepository.delete(id);
   }
 }

4e4770a3701b03879ec85a10c9efde93a7e5a60e
AlbaPCode <albapardo.code@gmail.com>
2024-03-10 18:25:07 +0100

    decorators fixed

--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -1,1 +1,24 @@
-export class Client {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+
+@Entity()
+export class Client {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  avatar
+
+  @Column({default:1})
+  isActive: number;
+}

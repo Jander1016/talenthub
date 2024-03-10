8b7d7b74766c4a9cc94e0a14d6c43421de338f15
Marcela3-DesFullStack <claudiamarcela.nc14@gmail.com>
2024-03-10 19:11:12 +0100

    Chore: changing folders and attach focused on next.js

---
+++ frontend/src/app/details/[talent_id]/page.tsx
@@ -0,0 +1,13 @@
+import CardDetails from '@/components/Cards/cardDetails'
+import React from 'react'
+
+
+const pageDetails = () => {
+    return (
+        <div>
+            <CardDetails />
+        </div>
+    )
+}
+
+export default pageDetails

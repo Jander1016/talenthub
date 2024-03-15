e9ad67aaa9ef0044d3182dcaf360106a7c94471c
AlbaPCode <albapardo.code@gmail.com>
2024-03-15 14:44:10 +0100

    dist for deploy api on Vercel addeded

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true, // Esta opción controla si se permite o no el envío de propiedades no definidas en tu DTO. Si está habilitada, solo se permitirán las propiedades definidas explícitamente en tu DTO. Cualquier propiedad adicional será eliminada automáticamente antes de que se pase al controlador. Esto ayuda a evitar que se pasen datos no deseados o inesperados a tu aplicación.
+    })
+  );
+//En resumen, app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true })) configura un ValidationPipe global que se aplicará a todas las solicitudes entrantes en tu aplicación NestJS. Este pipe se encargará de validar y transformar automáticamente las solicitudes entrantes según las reglas especificadas, lo que te ayuda a mantener tus controladores limpios y centrados en la lógica de negocio.
   await app.listen(3000);
 }
 

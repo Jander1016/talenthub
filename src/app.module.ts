a034e04ffad4fa3da7520597a414014d4f68283e
AlbaPCode <albapardo.code@gmail.com>
2024-03-15 07:10:40 +0100

    fix connexion errors resolved/fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 
-import { Admin } from './admins/entities/admin.entity'
+import { Admin } from './admins/entities/admin.entity';
 import { AdminsModule } from './admins/admins.module';
 
 import { Talent } from './talents/entities/talent.entity';
@@ -17,12 +17,11 @@ import { ClientsModule } from './clients/clients.module';
 import { Service } from './services/entities/service.entity';
 import { ServicesModule } from './services/services.module';
 
-
 import { Stack } from './stacks/entities/stack.entity';
 import { StacksModule } from './stacks/stacks.module';
 
-import { TalentsStack } from './talents_stacks/entities/talents_stack.entity'; //substacks
-import { TalentsStacksModule } from './talents_stacks/talents_stacks.module'; //substacks
+import { TalentsStack } from './talents_stacks/entities/talents_stack.entity';
+import { TalentsStacksModule } from './talents_stacks/talents_stacks.module';
 
 import { Wishlist } from './wishlists/entities/wishlist.entity';
 import { WishlistsModule } from './wishlists/wishlists.module';
@@ -30,19 +29,19 @@ import { WishlistsModule } from './wishlists/wishlists.module';
 import { DetailWishlist } from './detail_wishlist/entities/detail_wishlist.entity';
 import { DetailWishlistModule } from './detail_wishlist/detail_wishlist.module';
 
-
 config(); // Cargar variables de entorno desde el archivo .env
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE,
+      type: process.env.DB_TYPE as any, // Si no da error. Conversión explícita a cualquier tipo
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [Admin, Talent, Stack, TalentsStack, Service, Client, Wishlist, DetailWishlist],
+      entities: [Admin, Talent, Stack, TalentsStack, Service, Client, Wishlist, DetailWishlist], // Agrega aquí las demás entidades necesarias
       synchronize: false
     }),
 
@@ -69,9 +68,8 @@ config(); // Cargar variables de entorno desde el archivo .env
 
     DetailWishlist,
     DetailWishlistModule
+  ],
 
-    ],
-    
   controllers: [AppController],
   providers: [AppService],
 })
